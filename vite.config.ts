701c4ff2a2479a1af50e5565330f4289c77b536e
nupur das <nupur435das@gmail.com>
2026-01-25 14:31:47 +0530

    Fix build and production setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
+
+//import { defineConfig } from "vite";
+//import react from "@vitejs/plugin-react";
+//import path from "path";
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -9,7 +14,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '.'),
+      '@': path.resolve(__dirname, 'src'),
     }
   }
 })
